91f7d650259780f5bc97899ef51108ebbdfb9852
matheusvcv <matheusvcv@yahoo.com.br>
2025-05-26 22:21:01 -0300

    Realizado pratica em cs utilizando contantes e declorações de variaveis utilizando var e dynamic.

---
+++ Aula03/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Aula03;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 30;
+
+        number = 20;
+
+        Console.WriteLine("O valor da variável era igual a: " + number);
+
+        number = 10;
+
+        Console.WriteLine("O valor é igual a: " + number);
+
+        const int novoNumber = 1000;
+
+        Console.WriteLine("O valor da constante oe igual a: " + novoNumber);
+
+        var variable = 500;
+
+        var variable2 = "Texto";
+
+        Console.WriteLine(variable + "  " + "variable2");
+
+        dynamic var = 20;
+
+        Console.WriteLine("O valor da Dynamic era :" + var);
+
+        var = "Cachorro";
+
+        Console.WriteLine("O valor da Dynamic agora é :" + var);
+    }
+}
