dfea7599c11ce507fc68a2c971f8405a707ba703
matheusvcv <matheusvcv@yahoo.com.br>
2025-05-27 23:32:55 -0300

    Desenvolvido códigos da aula 04, sobre inputs e outputs. Apresentada a função ReadLine() e a conversão para string com Convert.toInt32().

---
+++ Aula04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("----------CADASTRO DE USUÁRIO----------");
+
+        Console.WriteLine("Digite Seu Nome:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite Sua Idade:");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("O nome é: " + nome + "e a idade é: " + idade);
+    }
+}
