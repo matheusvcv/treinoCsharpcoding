5e4df0864e66450dc97afe6da1b7d031cb28f127
matheusvcv <matheusvcv@yahoo.com.br>
2025-05-28 22:31:14 -0300

    Desenvolvido sistema que verifica se número é par ou impar, utilizando a estrutura condicional IF.

---
+++ Aula06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula06;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Descubra se o número é par ou ímpar =====");
+        Console.WriteLine("Digite um número: ");
+
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        int result = numero % 2;
+
+        if (result == 0)
+        {
+            Console.WriteLine("O número é  par!");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar!");
+        }
+    }
+}
