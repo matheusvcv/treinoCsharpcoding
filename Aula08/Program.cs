f4ee648bea27ea727321f0c1c6a8b8cb360acb22
matheusvcv <matheusvcv@yahoo.com.br>
2025-05-29 22:48:06 -0300

    Desenvolvida uma sequÊncia de estruturas condicionais para realizar testes com os operadores relacionais: == Operador de igualdade. != Operador de difereça > Operador maior que < Operador menor que >= Maior ou igual <= Menor ou igual

---
+++ Aula08/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Data;
+
+namespace Aula08;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Operadores Relacionais =====");
+
+        Console.WriteLine("\nDigite um número:");
+
+        int valor1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nDigite um número mais uma vez:");
+
+        int valor2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nOperador de Igualdade ==");
+
+        if (valor1 == valor2)
+        {
+            Console.WriteLine(valor1 + " é igual a " + valor2);
+        }
+        else
+        {
+            Console.WriteLine(valor1 + " é diferente " + valor2);
+        }
+
+        Console.WriteLine("\nOperador de diferença !=");
+
+        if (valor1 != valor2)
+        {
+            Console.WriteLine(valor1 + " é diferente de " + valor2);
+        }
+        else
+        {
+            Console.WriteLine(valor1 + " não é diferente de " + valor2);
+        }
+
+        Console.WriteLine("\nOperador Maior que >");
+
+        if (valor1 > valor2)
+        {
+            Console.WriteLine(valor1 + " é maior que " + valor2);
+        }
+        else
+        {
+            Console.WriteLine(valor1 + " não é maior que " + valor2);
+        }
+
+        Console.WriteLine("\nOperador Menor que <");
+
+        if (valor1 < valor2)
+        {
+            Console.WriteLine(valor1 + " é menor que " + valor2);
+        }
+        else
+        {
+            Console.WriteLine(valor1 + " não é menor que " + valor2);
+        }
+
+        Console.WriteLine("\nOperador Maior ou Igual");
+
+        if (valor1 >= valor2)
+        {
+            Console.WriteLine(valor1 + " é maior ou igual a " + valor2);
+        }
+        else
+        {
+            Console.WriteLine(valor1 + " não é maior ou igual a " + valor2);
+        }
+
+        Console.WriteLine("\nOperador Menor ou Igual");
+
+        if (valor1 <= valor2)
+        {
+            Console.WriteLine(valor1 + " é menor ou igual a " + valor2);
+        }
+        else
+        {
+            Console.WriteLine(valor1 + " não é menor ou igual a " + valor2);
+        }
+    }
+}
