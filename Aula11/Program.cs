5a6ffabe41387986b0b3cdf90b938abdc048f8ea
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-02 23:02:44 -0300

    Desenvolvidos testes e validações utilizando Switch. Levado em considerações variáveis do tipo int e bool.

---
+++ Aula11/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Aula11;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 2;
+
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine("\nO número é 1;");
+                break;
+
+            case 2:
+                Console.WriteLine("\nOnúmero é 2;");
+                break;
+        }
+
+        bool BoolValue = true;
+
+        switch (BoolValue)
+        {
+            case true:
+                Console.WriteLine("\nO valor é true;");
+                break;
+
+            case false:
+                Console.WriteLine("\nO valor é false;");
+                break;
+        }
+    }
+}
