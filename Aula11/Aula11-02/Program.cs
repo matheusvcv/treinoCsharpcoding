6870fadc4de7602ecaae4f764cba1b1112aee2f9
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-03 00:04:18 -0300

    Desenvolvido sistema que avalia notas de 0 a 10 em 5 níveis diferentes utilizando switch case.

---
+++ Aula11/Aula11-02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Aula11b;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=====Avaliação de Notas=====");
+
+        bool continua = true;
+
+        while (continua == true)
+        {
+            Console.WriteLine("Digite sua nota entre 0 e 10:");
+            int nota = Convert.ToInt32(Console.ReadLine());
+
+            switch (nota)
+            {
+                case 10:
+                case 9:
+                    Console.WriteLine("Seu desempenho foi ótimo.");
+                    break;
+
+                case 8:
+                case 7:
+                    Console.WriteLine("Seu desempenho foi bom.");
+                    break;
+
+                case 6:
+                case 5:
+                    Console.WriteLine("Seu desempenho foi regular");
+                    break;
+
+                case 4:
+                case 3:
+                    Console.WriteLine("Seu desempenho foi ruim");
+                    break;
+
+                case 2:
+                case 1:
+                case 0:
+                    Console.WriteLine("Seu desempenho foi abaixo do esperado.");
+                    break;
+
+                default:
+                    Console.WriteLine("O número que você digitou não é uam nota válida.");
+                    break;
+            }
+        }
+    }
+}
