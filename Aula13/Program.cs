2cdd4a7ba97aea97ee3e6bb3d5a8c9d7788c62ee
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-04 00:16:53 -0300

    Desenvolvido atividade do/while.

---
+++ Aula13/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula13;
+public class Program
+{
+    public static void Main()
+    {
+        int i = 101;
+
+        do
+        {
+            Console.WriteLine("O valor de i é: " + i);
+            i++;
+        } while (i <= 100);
+    }
+}
