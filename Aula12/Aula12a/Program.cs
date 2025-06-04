ccc5b2c941c5b707a15073d49272a81cde98eef3
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-03 23:12:41 -0300

    Desenvolvido simulador de conta bancaria tendo seu saldo utilizado.

---
+++ Aula12/Aula12a/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula12A;
+
+public class Program
+{
+    public static void Main()
+    {
+        int i = 100;
+
+        while (i >= 0)
+        {
+            Console.WriteLine("O valor do seu saldo é $" + i);
+            i--;
+        }
+    }
+}
