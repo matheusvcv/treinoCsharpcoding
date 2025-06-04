d2b90bb86e2edbc0332956d6491e1886e136fb42
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-03 23:04:04 -0300

    Desenvolvida estrutura de repetição while para itrar sobre contador.

---
+++ Aula12/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        int i = 0;
+
+        while (i <= 5)
+        {
+            Console.WriteLine("Loop Infinito! " + i);
+            i++;
+        }
+    }
+}
