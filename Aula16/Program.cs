f0df639101d5e6709f9ee30cbbba895f82df86e3
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-05 23:00:57 -0300

    Desenvolvida atividades e testes de Array.

---
+++ Aula16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula16;
+
+public class Program
+{
+    public static void Main()
+    {
+        string separator = "-----------------------";
+        string[] fruits = { "Apple", "Banana", "Cashew", "Mango" };
+
+        Console.WriteLine(fruits[0]);
+        Console.WriteLine(separator);
+        Console.WriteLine(fruits[3]);
+        Console.WriteLine(separator);
+
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
