b2edd2997e5e69f05afe47878901a74dffd844f6
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-09 23:07:52 -0300

    Desenvolvido listas com intuito da pratica de conceitos e entendimento geral do funcionamento.

---
+++ Aula17/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Aula18;
+
+public class Program
+{
+    public static void Main()
+    {
+        string separator = "~~''''~~''''~~''''~~''''~~~''''~~~''''~~";
+
+        Console.WriteLine(separator);
+
+        List<string> lordes = new List<string>()
+        {
+            "Stark",
+            "Lannister",
+            "Tully",
+        };
+
+        Console.WriteLine(lordes[0]);
+
+        Console.WriteLine(separator);
+
+        foreach (string lorde in lordes)
+        {
+            Console.WriteLine(lorde);
+        }
+
+        Console.WriteLine(separator);
+
+        lordes.Add("Tyrel");
+        lordes.Add("Martel");
+
+        foreach (string lorde in lordes)
+        {
+            Console.WriteLine(lorde);
+        }
+
+        Console.WriteLine(separator);
+    }
+}
