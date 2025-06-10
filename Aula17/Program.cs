1df9d0d3783acaa67dd05219f7cd5dccd1963929
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-09 23:26:08 -0300

    Desenvolvidos testes utilizando os comandos: ass, remove, count. Realizado foreach nas listas e acesso e substituição dos dados.

--- Aula17/Program.cs
+++ Aula17/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Aula18;
+﻿using System.Runtime.CompilerServices;
+
+namespace Aula18;
 
 public class Program
 {
@@ -29,6 +31,40 @@ public class Program
         lordes.Add("Tyrel");
         lordes.Add("Martel");
 
+        foreach (string lorde in lordes)
+        {
+            Console.WriteLine(lorde);
+        }
+
+        Console.WriteLine(separator);
+
+        lordes.Remove("Lannister");
+        lordes.Remove("Arryn");
+
+        foreach (String lorde in lordes)
+        {
+            Console.WriteLine(lorde);
+        }
+
+        Console.WriteLine(separator);
+
+        Console.WriteLine(lordes.Count);
+
+        Console.WriteLine(lordes[3]);
+
+        Console.WriteLine(separator);
+
+        lordes[1] = "Blackwood";
+
+        foreach (String lorde in lordes)
+        {
+            Console.WriteLine(lorde);
+        }
+
+        Console.WriteLine(separator);
+
+        lordes.Clear();
+       
         foreach (string lorde in lordes)
         {
             Console.WriteLine(lorde);
