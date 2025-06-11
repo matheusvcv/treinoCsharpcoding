41349fe327890a8d015fa23a7d1676bd0ce32705
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-10 23:15:25 -0300

    Desenvoolvida nova forma de criar arquivos utilizando variaveis para definir o caminho específico.

--- Aula18/Program.cs
+++ Aula18/Program.cs
@@ -6,5 +6,9 @@ public class Program
     public static void Main()
     {
         File.WriteAllText("file.txt", "Hello, world.");
+
+        string path = @"C:\Users\mathe\OneDrive\Área de Trabalho\CSharpProjetos\Aula18\fileName.txt";
+        string text = "Hello world!";
+        File.WriteAllText(path, text);
     }
-}
+ }
