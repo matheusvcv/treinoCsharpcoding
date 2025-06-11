1d78646d1d7c5e28240320f836cba7924be12666
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-11 00:02:50 -0300

    Utilizado a função ReadAllText para ler arquivos.

--- Aula18/Program.cs
+++ Aula18/Program.cs
@@ -10,5 +10,20 @@ public class Program
         string path = @"C:\Users\mathe\OneDrive\Área de Trabalho\CSharpProjetos\Aula18\fileName.txt";
         string text = "Hello world!";
         File.WriteAllText(path, text);
+
+        string pathA = @"C:\Users\mathe\OneDrive\Área de Trabalho\CSharpProjetos\Aula18\";
+        string fileName = "MeuArquivo.Doc";
+
+        string filePath = pathA + fileName;
+
+        string conteudoAdicional = " o Rato roeu a roupa do rei de Roma.";
+
+        string conteudoAdicional2 = " novo texto verdade adicional teste.";
+
+        File.AppendAllText(filePath, conteudoAdicional);
+
+        File.AppendAllText(filePath, conteudoAdicional2);
+
+        File.ReadAllText(filePath);
     }
- }
+}
--- Aula18/obj/Debug/net9.0/Aula18.AssemblyInfo.cs
+++ Aula18/obj/Debug/net9.0/Aula18.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula18")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c1f3720288f46f8de5889a7b49c81ee1e4b6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41349fe327890a8d015fa23a7d1676bd0ce32705")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula18")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula18")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
