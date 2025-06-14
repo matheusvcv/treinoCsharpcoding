3714e940493dafdc7b2c929e6c0cff1c4e347b1b
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-14 17:50:43 -0300

    Desenvolvido uma nova classe, com atributo e executado o atributo no terminal.

---
+++ Aula23/Bank.cs
@@ -0,0 +1,12 @@
+namespace Aula23;
+
+public class Bank
+{
+    public int Id;
+    public string Name;
+
+    public void RequestNewCard()
+    {
+        Console.WriteLine("Solicitando cartão. E-mail enviando...");
+    }
+}
---
+++ Aula23/Program.cs
@@ -0,0 +1,15 @@
+﻿using Aula23;
+
+namespace Aula;
+
+public class Program
+{
+    public static void Main()
+    {
+        var BancoPraiano = new Bank();
+        BancoPraiano.Id = 12;
+        BancoPraiano.Name = "Banco Praiano";
+
+        BancoPraiano.RequestNewCard();
+    }
+}
