ae7fa72b03306a96f34cd59203b330a0b2c310b4
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-16 22:59:47 -0300

    Desenvolvida classe com metodo que retorna um parametro. Desenvolvida lógica de retorno.

---
+++ Aula24/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula24;
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person();
+        Console.WriteLine("Digite a idade: ");
+
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        bool IfLegalPerson = person.IfLegalPerson(age);
+
+        if (IfLegalPerson == true)
+        {
+            Console.WriteLine("A pessoa é menor de idade!");
+        }
+        else
+        {
+            Console.WriteLine("A pessoa é maior de idade!");
+        }
+    }
+}
