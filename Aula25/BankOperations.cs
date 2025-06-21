5a92fbed77122d5ba1ae80dda50a42beb76a94ad
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-20 22:52:20 -0300

    Desenvolvido novo méto (WithDraw) para realizar saques.

--- Aula25/BankOperations.cs
+++ Aula25/BankOperations.cs
@@ -11,10 +11,30 @@ public class BankOperations()
 
     public void Depoisit()
     {
-        Console.WriteLine("Informe o valor a ser depositado: R$ ");
-        if (decimal.TryParse(Console.ReadLine(), out decimal amount ) && amount > 0)
+        Console.WriteLine("Informe o valor a ser depositado:");
+        if (decimal.TryParse(Console.ReadLine(), out decimal amount) && amount > 0)
         {
             balance += amount;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido. Tente novamente.");
+        }
+    }
+
+    public void WithDraw()
+    {
+        Console.WriteLine("Informe o valor que você gostaria de sacar:");
+        if (decimal.TryParse(Console.ReadLine(), out decimal amount) && amount > 0)
+        {
+            if (amount <= balance)
+            {
+                Console.WriteLine($"O valor de R$ {amount} retirado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
         }
         else
         {
