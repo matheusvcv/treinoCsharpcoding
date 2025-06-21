8eedd089bc433cd1e16ff2d24c3572786787d1ea
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-21 13:54:10 -0300

    Chamada a instancia do terminal no program-> main; Executado sem erros. Programa bancário finalizado.

--- Aula25/Program.cs
+++ Aula25/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aula25;
+
+public class Program
+{
+    public static void Main()
+    {
+        BankTerminal terminal = new BankTerminal();
+
+        terminal.start();
+    }
+}
