9af0dc1b9d9d690997388178588d6521ab98c9c8
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-22 16:39:06 -0300

    Desenvolvido todos os stributos da classe 'Cake'. Dentro do construtor da classe declarados os valores desses atributos.

--- Aula26/Aula26.cs
+++ Aula26/Aula26.cs
@@ -1,9 +1,21 @@
+using System.Drawing;
+
 namespace Aula26;
 
 public class Cake
 {
+    public string Flavor;
+    public string Filling;
+    public string Coverage;
+    public int Layers;
+    public string  Size;
+
     public Cake()
     {
-        
+        Flavor = "Chocolate";
+        Filling = "Coconut";
+        Coverage = "Chocolate";
+        Layers = 3;
+        Size = "Médio";
     }
 }
