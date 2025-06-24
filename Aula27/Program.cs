21bbd3db60c40eeef327eceb19efa5d30ff20142
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-23 22:59:55 -0300

    Realizado teste de instaciação de classe passando atributos por parametor.

--- Aula27/Program.cs
+++ Aula27/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+namespace Aula27;
+
+public class Program
+{
+    public static void Main()
+    {
+        Cake ChocolateCake = new Cake("Morango", "Chocolate", "Coco", 3, "Grande");
+
+        Console.WriteLine(ChocolateCake.Flavor);
+        Console.WriteLine(ChocolateCake.Filling);
+        Console.WriteLine(ChocolateCake.Coverage);
+        Console.WriteLine(ChocolateCake.Layers);
+        Console.WriteLine(ChocolateCake.Size);
+    }
+}
